67b80c1bc663aa135d9d97650e077fabfdd199b9
Matthew Soulanille <matthew@soulanille.net>
2019-03-21 15:44:09 -0400

    + and - are swapped. This is done by checking for them as literals during the code's runtime phase.

--- boxer-unboxer.rkt
+++ boxer-unboxer.rkt
@@ -1,7 +1,12 @@
 #lang racket
 (provide boxer-unboxer)
 (require syntax/parse syntax/strip-context)
-
+(require syntax/kerncase)
+(define-syntax-class facet
+  ;#:literals (facet)
+  (pattern [(~literal facet) guard private public]
+           
+           ))
 
 
 (define (boxer-unboxer stx)
@@ -9,13 +14,21 @@
     ; These need to reference the literal + and - functions or they'll be tricked
     ; by redefinitions. Sadly, when they point to the literal + and - es of this
     ; context, they can't see the ones of the context of stx if stx has different context.
-    [(~literal -) (let () (print "got a minus") #'+)]
-    [(~literal +) #'-]
-    [(~var t id) #'"asdf"]
+    [fac:facet #'fac.public]; same as (attribute fac.public)?
+    ; #:phase -1 indicates that we want to go one level down in phase
+    ; higher up means more macro-ey and lower down means more code-we're-executing-ey
+    ; to put it in super impercise terms. All phase levels are relative to the code you're
+    ; executing with that code being at phase level 0. Look it up for more detail.
+    [(~literal - #:phase -1) #'+]
+    [(~literal + #:phase -1) #'-]
+    ;[(~literal +) #'-]
+    ;[(~var t id) #'"asdf"]
     [(~datum :test) #'(print "yep, working")]
     ; Recursively call this on all the syntax
     ; Without replace-context, things like #%app get lost and functions don't work
-    [(any ...) (replace-context stx #`(#,@(map boxer-unboxer (syntax-e #`(any ...)))))]
+    [(any ...)
+     #:with recursed #`(#,@(map boxer-unboxer (syntax-e #`(any ...))))
+    (replace-context stx #'recursed)]
     [default #'default]
   )
   )
