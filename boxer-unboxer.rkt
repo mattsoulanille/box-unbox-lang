8842268713d255f500aebf1b3b20feae64dba65e
Matthew Soulanille <matthew@soulanille.net>
2019-03-20 23:35:15 -0400

    Replaces all identifiers with 'asdf'

--- boxer-unboxer.rkt
+++ boxer-unboxer.rkt
@@ -2,10 +2,16 @@
 (provide boxer-unboxer)
 (require syntax/parse syntax/strip-context)
 
+
+
 (define (boxer-unboxer stx)
   (syntax-parse stx
-    [(~datum -) (let () (print "got a minus") #'+)]
-    [(~datum +) #'-]
+    ; These need to reference the literal + and - functions or they'll be tricked
+    ; by redefinitions. Sadly, when they point to the literal + and - es of this
+    ; context, they can't see the ones of the context of stx if stx has different context.
+    [(~literal -) (let () (print "got a minus") #'+)]
+    [(~literal +) #'-]
+    [(~var t id) #'"asdf"]
     [(~datum :test) #'(print "yep, working")]
     ; Recursively call this on all the syntax
     ; Without replace-context, things like #%app get lost and functions don't work
