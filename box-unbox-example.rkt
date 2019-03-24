c8b8f7f7e7e369409fb9bdf8400e3ba7e0fc307c
Matthew Soulanille <matthew@soulanille.net>
2019-03-24 15:55:54 -0400

    define-values and let-values seem to box correctly

--- box-unbox-example.rkt
+++ box-unbox-example.rkt
@@ -1,23 +1,25 @@
 #lang s-exp "box-unbox-module.rkt"
 
-(- 4 5)
+;(- 4 5)
 (let ([x 4] [y 5])
   (+ x y)
   )
 
-(facet (lambda (x) #t) 1 3)
-(let ([minus -])
-  (minus 50 50)
-  )
+;(facet (lambda (x) #t) 1 3)
+;(let ([minus -])
+;  (minus 50 50)
+;  )
 
-(define (test a b)
-  (+ (* a b) (- a b)))
+;(define (test a b)
+;  (+ (* a b) (- a b)))
 
 
 
 (define x 12)
+(print x)
+;(let ([x 5])
+;  (+ x 4)
+;  )
+;(map + (list 1 2 3))
 
-(let ([x 5])
-  
-  (+ x 4)
-  )
+;(require macro-debugger/syntax-browser)
