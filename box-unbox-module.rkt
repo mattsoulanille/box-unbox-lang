f44bb39f9636597e66f1ed0e725af2abd16c74d9
Matthew Soulanille <matthew@soulanille.net>
2019-03-24 14:48:40 -0400

    successfully local-expanding the entire input

--- box-unbox-module.rkt
+++ box-unbox-module.rkt
@@ -9,10 +9,20 @@
 (provide (except-out (all-from-out racket) #%module-begin)
          (rename-out [box-unbox-module-begin #%module-begin]))
 
+
+;https://lexi-lambda.github.io/blog/2018/04/15/reimplementing-hackett-s-type-language-expanding-to-custom-core-forms-in-racket/
+; We don't do a local-expand here since that means
+; we wouldn't be able to parse any syntax that is
+; custom to our language. It would only support racket stuff.
 (define-syntax (box-unbox-module-begin stx)
   (let ([result
          (syntax-parse stx
-           [(module-begin expr ...) #`(#%module-begin #,@(boxer-unboxer #`(expr ...)))]
+           ; plain-module-begin from LAL p5
+           [(module-begin expr ...)
+            #:with core-forms (local-expand #`(#%plain-module-begin expr ...)
+                                            'module-begin '())
+            #'core-forms
+            ]
            )
          ])
     (print result)
