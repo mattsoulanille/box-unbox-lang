9f4546dd1cf1e29db12963e55d851be6803019ec
Matthew Soulanille <matthew@soulanille.net>
2019-03-24 20:54:08 -0400

    Remove old code

--- boxer-unboxer.rkt
+++ boxer-unboxer.rkt
@@ -5,58 +5,6 @@
 
 (require (for-syntax syntax/parse))
 
-(define-syntax-class facet
-  ;#:literals (facet)
-  (pattern [(~literal facet #:phase -1) guard private public]
-           ))
-
-
-; How do I make this not produce false positives
-(define (was-boxed? boxed-vars to-check)
-  (if (identifier? to-check)
-      (foldl (lambda (a b) (or a b)) #f (map (lambda (var) (bound-identifier=? to-check var -1)) boxed-vars))
-      #f
-      )
-  )
-
-(define (boxer-unboxer-helper boxed-vars stx)
-  (let ([new-boxed-vars boxed-vars])
-    (cons
-     (syntax-parse stx
-       ; These need to reference the literal + and - functions or they'll be tricked
-       ; by redefinitions. Sadly, when they point to the literal + and - es of this
-       ; context, they can't see the ones of the context of stx if stx has different context.
-       ; Addendum: Phases fix this. See below.
-       ;[fac:facet #'fac.public]; same as (attribute fac.public)?
-       ; #:phase -1 indicates that we want to go one level down in phase
-       ; higher up means more macro-ey and lower down means more code-we're-executing-ey
-       ; to put it in super impercise terms. All phase levels are relative to the code you're
-       ; executing with that code being at phase level 0. Look it up for more detail.
-       ;[(~literal - #:phase -1) #'+]
-       ;[(~literal + #:phase -1) #'-]
-       ;[(~datum :test) #'(print "yep, working")]
-       ; Recursively call this on all the syntax
-       ; Without replace-context, things like #%app get lost and functions don't work
-
-
-       [((~literal define #:phase -1) var:identifier val:expr)
-        (set! new-boxed-vars (set-add boxed-vars #'var))
-        (replace-context stx #'(define var (box val)))
-        ]
-     
-       [(any ...)
-        ; Todo: Make this pass the boxed-vars set correctly!
-        #:with recursed (map car (map (curry boxer-unboxer-helper boxed-vars) (syntax-e #`(any ...))))
-        (replace-context stx #'recursed)]
-       ; todo: Check if it's actually being used as a variable. But how?
-       [single (if (was-boxed? (set->list boxed-vars) #'single)
-                   #'(unbox single)
-                   #'single)]  ;(let () (print #'single) #'single))]
-       ;[default #'default]
-       )
-     new-boxed-vars)
-  ))
-
 (define (box-stx stx)
   #`(box #,stx))
 (define (unbox-stx stx)
@@ -79,9 +27,6 @@
      #'(apply values res-list)]
     )
   )
-     
-      
-  
 
 ; See https://docs.racket-lang.org/reference/syntax.html
 ; for a list of all racket/base syntactic forms.
